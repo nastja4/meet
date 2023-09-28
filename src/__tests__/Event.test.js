c57d4d69f278304a4d175bea83a54778c6f5486b
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-09-29 00:43:24 +0200

    Add tests for Feat 2, Events.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,62 @@
+import Event from "../components/Event";
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import mockData from "../mock-data";
+import { getEvents } from '../api';
+
+// const mockEvent = mockData[0]; // using a specific event from mockData for testing
+
+describe('<Event /> component', () => {
+  let EventComponent;
+  let allEvents; // Declare allEvents variable    
+
+  beforeEach(async () => {
+    allEvents = await getEvents(); // Fetch events before each test
+    EventComponent = render(<Event event={allEvents[0]} />);
+  });
+
+  // Feature 2: Show/Hide Event Details
+  // Scenario 1: Given an event is displayed in the event list,
+  // When the user views the event,
+  // Then the event details should be collapsed by default.
+  test('renders event`s title, event details should be collapsed by default', () => {
+    expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
+  });
+
+  test('renders event`s start time', () => {
+    expect(EventComponent.queryByText(allEvents[0].created)).toBeInTheDocument();
+  });
+
+  test('renders event`s location', () => {
+    expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
+  });
+
+  test('renders event details btn with the title (Show Details)', () => {
+    expect(EventComponent.queryByText('Show Details')).toBeInTheDocument();
+  });
+
+  test('by default, event`s details section should be hidden', () => {
+    const detailsSection = EventComponent.container.querySelector(".eventDetails");
+    expect(detailsSection).toBeNull();
+  });
+
+  test('shows the details section when the user clicks on the `Show Details` btn', async () => {
+    const user = userEvent.setup();
+    await user.click(EventComponent.getByText("Show Details"));
+
+    expect(EventComponent.container.querySelector(".eventDetails")).toBeInTheDocument();
+    expect(EventComponent.queryByText('Show Details')).toBeNull();
+    expect(EventComponent.queryByText('Hide Details')).toBeInTheDocument();
+  });
+
+  test('hides the details section when the user clicks on the `Hide Details` btn', async () => {
+    const user = userEvent.setup();
+    await user.click(EventComponent.getByText("Show Details"));
+    await user.click(EventComponent.getByText("Hide Details"));
+
+    expect(EventComponent.container.querySelector(".eventDetails")).toBeNull();
+    expect(EventComponent.getByText("Show Details")).toBeInTheDocument();
+  });
+
+
+});
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,40 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  // Function to format a date string to a human-readable format
+  const formatDate = (dateTimeString) => {
+    const date = new Date(dateTimeString);
+    return date.toLocaleString(); // Adjust the format as needed
+  };
+
   return (
-    <li></li>
+    <li className="event">
+      <b>{event.summary}</b>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      
+      {showDetails ? (
+        <div className="eventDetails">
+          <b>Event Details</b>
+          <p>Location: {event.location}</p>
+          <p>Start Time: {formatDate(event.start.dateTime)}</p>
+          <p>End Time: {formatDate(event.end.dateTime)}</p>
+          <p>{event.status}</p>
+          <p>Description: {event.description}</p>            
+        </div>
+      ) : null}
+
+      <button className="eventDetails-btn"
+        onClick={() => {
+          setShowDetails(!showDetails); // Toggle the state
+        }}
+      >
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
+    </li>    
   );
-}
+};
 
 export default Event;
